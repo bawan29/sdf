07439233e25458785d1d68b3ef2419f970fbb4e3
bawan29 <b02853863@gmail.com>
2025-03-06 09:14:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
+require('dotenv').config()
+
 const express = require("express");
 const { createServer } = require("node:http");
 const { Server } = require("socket.io");
-
-const path = require("path");
 
 const app = express();
 const server = createServer(app);
@@ -15,6 +15,8 @@ const io = new Server(server, {
   },
 });
 
+const PORT = process.env.PORT || 8000
+
 const users = new Map();
 
 function sendLogToClients(message) {
@@ -23,39 +25,29 @@ function sendLogToClients(message) {
 
 // Middleware to filter based on `user-agent`
 
-io.use((socket, next) => {
-  const userAgent = socket.handshake.headers["user-agent"] || "";
-
-  // Check if the user-agent is from a mobile device
-  const isPhone = /android|iphone|ipad|mobile/i.test(userAgent);
-
-  if (isPhone) {
-    next(); // Allow connection
-  } else {
-    const err = new Error("Connection not allowed for non-mobile devices");
-    err.data = { status: 403, message: "Forbidden" };
-    next(err); // Reject connection with an error
-  }
-});
 
 io.on("connection", (socket) => {
   sendLogToClients(`User connected: ${socket.id}`);
+  console.log(`User connected: ${socket.id}`);
 
   socket.on("register", (userId) => {
     users.set(userId, socket.id);
     sendLogToClients(users);
     sendLogToClients(`User registered: ${userId} => ${socket.id}`);
+    console.log(`User registered: ${userId} => ${socket.id}`)
   });
 
   sendLogToClients(users);
+  console.log(users);
 
   socket.on("private_message", ({ receiverId, message }) => {
     const receiverSocketId = users.get(receiverId);
     if (receiverSocketId) {
       io.to(receiverSocketId).emit("message", message);
       sendLogToClients(`Message sent to ${receiverSocketId}: ${message}`);
+      console.log(`Message sent to ${receiverSocketId}: ${message}`);
     } else {
-      sendLogToClients(`User ${receiverId} is not connected.`);
+      console.log(`USER ${receiverId} IS NOT CONNECTED.`);
     }
   });
 
@@ -66,6 +58,7 @@ io.on("connection", (socket) => {
         users.delete(userId);
         sendLogToClients(`User removed: ${userId}`);
         sendLogToClients(users);
+        console.log(`User removed: ${userId}`);
         break;
       }
     }
@@ -76,4 +69,4 @@ app.get("/", (req, res) => {
   return res.status(400).send("<pre>Cannot GET /</pre>");
 });
 
-server.listen(9000, () => console.log("Server Start at port: 9000"));
+server.listen(PORT, () => console.log(`Server Start at port: ${PORT}`));
